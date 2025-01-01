20837d44e443f3760cd3a18f0fba54df7ec995aa
JenChieh <jcs090218@gmail.com>
2025-01-01 01:03:16 -0800

    Update copyright year

--- flycheck-dart.el
+++ flycheck-dart.el
@@ -1,7 +1,7 @@
 ;;; flycheck-dart.el --- Flycheck for dart analyze  -*- lexical-binding: t; -*-
 
 ;; Copyright (C) 2015  martin haesler
-;; Copyright (C) 2024  Shen, Jen-Chieh
+;; Copyright (C) 2024-2025  Shen, Jen-Chieh
 
 ;; Author: Shen, Jen-Chieh <jcs090218@gmail.com>
 ;; Maintainer: Shen, Jen-Chieh <jcs090218@gmail.com>
